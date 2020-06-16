6820ee18d06fd42113083228f2b39ed229831561
Matt <mgmcintyre777@gmail.com>
2020-06-16 15:09:16 -0400

    fixed query error

--- src/main/java/com/smoothstack/gcfashion/dao/StoreDAO.java
+++ src/main/java/com/smoothstack/gcfashion/dao/StoreDAO.java
@@ -12,6 +12,6 @@ import org.springframework.data.jpa.repository.Query;
 public interface StoreDAO extends JpaRepository<Store, Long> {
 	
 	@Query(value = "select s.store_name, SUM(t.total) from store s left join transaction t on s.store_id = t.store_id group by store_name", nativeQuery = true)
-	Object getSalesReport();
+	List<Object> getSalesReport();
 	
 }
